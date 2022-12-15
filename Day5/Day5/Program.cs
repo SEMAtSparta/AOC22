47fa71c69bb53525a86e0d687f9bb7a3351b7915
Scot Morrison <SMorrison@spartaglobal.com>
2022-12-15 14:45:07 +0000

    Partial solution for day 5.

--- Day5/Day5/Program.cs
+++ Day5/Day5/Program.cs
@@ -5,26 +5,39 @@ public class Program
 {
     static void Main(string[] args)
     {
-        string[] input = File.ReadAllLines("input.txt");
-        ParseInputToStackAndInstructions(input, 9, 10, out string[] instructions);
+        Part1();
     }
 
-    static Stack<string> ParseInputToStackAndInstructions(string[] inputStrings, int numberOfPiles, int maxSizeOfStack, out string[] instructions)
+    public static void Part1()
     {
-        for (int i = 0; i < maxSizeOfStack; i++)
+        string[] input = File.ReadAllLines("input.txt");
+        List<Instruction> listOfInstructions = new();
+        Stack<string>[] arrayOfPiles = ParseInputToStackAndInstructions(input, 9, 8, out listOfInstructions);
+
+        foreach (Instruction i in listOfInstructions)
         {
-            //sort out the crates
+            MoveCargo(i.numberOfCrates, i.initialLocation, i.targetLocation, ref arrayOfPiles);
         }
+
+        foreach (Stack<string> pile in arrayOfPiles)
+        {
+            Console.WriteLine(pile.Peek());
+        }
+    }
+    static Stack<string>[] ParseInputToStackAndInstructions(string[] inputStrings, int numberOfPiles, int maxSizeOfStack, out List<Instruction> instructions)
+    {
+        Stack<string>[] arrayOfPiles = ParseStringToArrayOfPiles(inputStrings, maxSizeOfStack);
 
         List<Instruction> listOfInstructions = new();
         //skip the two dead lines in the input
-        for(int i = maxSizeOfStack + 2; i < inputStrings.Length; i++)
+        for (int i = maxSizeOfStack + 2; i < inputStrings.Length; i++)
         {
             //sort out the instructions
             listOfInstructions.Add(ParseStringToInstruction(inputStrings[i]));
         }
 
-        throw new NotImplementedException();
+        instructions = listOfInstructions;
+        return arrayOfPiles;
     }
 
     static Instruction ParseStringToInstruction(string input)
@@ -35,10 +48,45 @@ public class Program
         int numberOfCrates = int.Parse(splitInput[0].Trim().Split(" ")[1]);
 
         string[] locations = splitInput[1].Replace("from", "").Replace(" ", "").Split("to");
-        int initialLocation = int.Parse(locations[0]);
-        int targetLocation = int.Parse(locations[1]);
+        //subtract 1 to make the locations match with array indexes
+        int initialLocation = int.Parse(locations[0]) - 1;
+        int targetLocation = int.Parse(locations[1]) - 1;
 
         return new Instruction(initialLocation, targetLocation, numberOfCrates);
+    }
+
+    public static Stack<string>[] ParseStringToArrayOfPiles(string[] inputStrings, int maxSizeOfStack)
+    {
+        Stack<string>[] arrayOfPiles = new Stack<string>[9];
+        for (int i = 0; i < arrayOfPiles.Length; i++)
+        {
+            arrayOfPiles[i] = new Stack<string>();
+        }
+
+        for (int i = 0; i < maxSizeOfStack; i++)
+        {
+            //sort out the crates
+            for (int j = 0; j < inputStrings[i].Length; j += 4)
+            {
+                if (!inputStrings[i].Substring(j, 3).StartsWith("["))
+                {
+                    continue;
+                }
+                int pileIndex = j / 4;
+                arrayOfPiles[pileIndex].Push(inputStrings[i].Substring(j, 3));
+            }
+        }
+
+        return arrayOfPiles;
+    }
+
+    public static void MoveCargo(int numberOfCrates, int initialLocation, int targetLocation, ref Stack<string>[] arrayOfPiles)
+    {
+        for(int i = 0; i < numberOfCrates; i++)
+        {
+            string crate = arrayOfPiles[initialLocation].Pop();
+            arrayOfPiles[targetLocation].Push(crate);
+        }
     }
 }
 
