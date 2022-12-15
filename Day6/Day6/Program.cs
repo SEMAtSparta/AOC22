f346997645d309c9b92e63f0bf7100c36582de18
Scot Morrison <SMorrison@spartaglobal.com>
2022-12-15 16:07:02 +0000

    Added partial solution for day 6.

--- Day6/Day6/Program.cs
+++ Day6/Day6/Program.cs
@@ -1,17 +1,52 @@
-﻿using System.IO;
+﻿using System.Drawing;
+using System.IO;
 namespace Day6;
 
-internal class Program
+public class Program
 {
     static void Main(string[] args)
     {
-        string inputString = File.ReadLine("input.txt");  
-        
+        string[] inputString = File.ReadAllLines("input.txt");
+        Console.WriteLine(FindStartOfPacketMarker(inputString[0], 4));
+        Console.WriteLine(FindStartOfPacketMarker(inputString[0], 14));
+
 
     }
 
-    public int FindStartOfPacketMarker(string)
+    public static int FindStartOfPacketMarker(string dataStreamBuffer, int sizeOfMarker)
     {
+        Queue<char> packet = new();
+        int currentPosition = 0;
+        foreach(char c in dataStreamBuffer)
+        {
+            packet.Enqueue(c);
+            currentPosition++;
+            if (packet.Count == sizeOfMarker)
+            {
+                if (AreStackContentsDifferent(packet))
+                {
+                    return (currentPosition);
+                }
+                packet.Dequeue();
+            }
+        }
+        return -1;
+    }
 
+    public static bool AreStackContentsDifferent(Queue<char> packet)
+    {
+        Queue<char> packetCopy = new Queue<char>(packet);
+        for(int i = 0; i < packetCopy.Count+1; i++)
+        {
+            char c = packetCopy.Dequeue();
+            foreach(char item in packetCopy)
+            {
+                if(c == item)
+                {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 }
--- Day6/ParseStringTests/UnitTest1.cs
+++ Day6/ParseStringTests/UnitTest1.cs
@@ -3,13 +3,20 @@ namespace ParseStringTests;
 
 public class Tests
 {
-    [TestCase("bvwbjplbgvbhsrlpgdmjqwftvncz", 5)]
-    [TestCase("nppdvjthqldpwncqszvftbrmjlhg", 6)]
-    [TestCase("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 10)]
-    [TestCase("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 11)]
+    [TestCase("bvwbjplbgvbhsrlpgdmjqwftvncz",4, 5)]
+    [TestCase("nppdvjthqldpwncqszvftbrmjlhg",4, 6)]
+    [TestCase("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg",4, 10)]
+    [TestCase("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw",4, 11)]
+    [TestCase("mjqjpqmgbljsphdztnvjfqwrcgsmlb", 14, 19)]
+    [TestCase("bvwbjplbgvbhsrlpgdmjqwftvncz", 14, 23)]
+    [TestCase("nppdvjthqldpwncqszvftbrmjlhg", 14, 23)]
+    [TestCase("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 14, 29)]
+    [TestCase("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 14, 26)]
 
-    public void GivenString_FindStartOfPacketMarker_ReturnsPosition(string input, int expectedResult)
+    
+
+    public void GivenString_FindStartOfPacketMarker_ReturnsPosition(string input, int sizeOfMarker, int expectedResult)
     {
-        Assert.That(Program.FindStartOfPacketMarker(input), Is.EqualTo(expectedResult));
+        Assert.That(Program.FindStartOfPacketMarker(input, sizeOfMarker), Is.EqualTo(expectedResult));
     }
 }
