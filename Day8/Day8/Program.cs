874ca25d04209075778c9ffc8f598bfcdaff0ff7
SEMAtSparta <scot@villain.love>
2022-12-26 16:22:16 +0000

    Added partial solution for day 8.

--- Day8/Day8/Program.cs
+++ Day8/Day8/Program.cs
@@ -4,6 +4,75 @@ public class Program
 {
     static void Main(string[] args)
     {
+        string[] inputStrings = File.ReadAllLines("input.txt");
+        List<int[]> input2DArray = StringArrayTo2DInts(inputStrings);
+        Console.WriteLine(FindVisibleTrees(input2DArray)); 
+    }
 
+    public static List<int[]> StringArrayTo2DInts(string[] inputStrings)
+    {
+        List<int[]> output = new List<int[]>();
+        for(int i = 0; i < inputStrings.Length; i++)
+        {
+            output.Add(StringToIntArray(inputStrings[i]));
+        }
+        return output;
+    }
+
+    public static int[] StringToIntArray(string input)
+    {
+        char[] inputToChar = input.ToCharArray();
+        int[] output = new int[inputToChar.Length];
+        for (int i = 0; i < output.Length; i++)
+        {
+            string s = inputToChar[i].ToString();
+            output[i] = Int32.Parse(s);
+        }
+
+        return output;
+    }
+
+    public static int FindVisibleTrees(List<int[]> input2D)
+    {
+        int sum = (input2D.Count * 2) + ((input2D[0].Length - 2) * 2);
+        for(int col = 1; col < input2D.Count-1; col++)
+        {
+            for(int row = 1; row < input2D[0].Length-1; row++)
+            {
+                sum += CheckVisibility(input2D, row, col) ? 1 : 0;
+            }
+        }
+
+        return sum;
+    }
+
+    public static bool CheckVisibility(List<int[]> input2D, int row, int col)
+    {
+        bool[] visibleFromDirections = new bool[4];
+        int target = input2D[col][row];
+
+        visibleFromDirections[0] = CheckOneDirection(input2D, target, 0, col, true, row);
+        visibleFromDirections[1] = CheckOneDirection(input2D, target, col+1, input2D.Count, true, row);
+        visibleFromDirections[2] = CheckOneDirection(input2D, target, 0, row, false, col);
+        visibleFromDirections[3] = CheckOneDirection(input2D, target, row+1, input2D[col].Length, true, col);
+        
+        foreach(bool b in visibleFromDirections)
+        {
+            if( b ) return true;
+        }
+        return false;
+    }
+
+    public static bool CheckOneDirection(List<int[]> input2D, int target, int start, int end, bool column, int fixedDimension)
+    {
+        for (int i = start; i < end; i++)
+        {
+            int tree = column ? input2D[i][fixedDimension] : input2D[fixedDimension][i];
+            if (tree >= target)
+            {
+                return false;
+            }
+        }
+        return true;
     }
 }
---
+++ Day8/Day8Tests/Day8Tests.cs
@@ -0,0 +1,54 @@
+using Day8;
+
+namespace Day8Tests;
+
+public class Tests
+{
+    [Test]
+    public void GivenStringArray_Return2DIntArray()
+    {
+        string[] input = {
+            "30373",
+            "25512",
+            "65332",
+            "33549",
+            "35390"
+        };
+
+        List<int[]> ints = Program.StringArrayTo2DInts(input);
+        Assert.That(ints[0][2], Is.EqualTo(3));
+        Assert.That(ints[2][1], Is.EqualTo(5));
+        Assert.That(ints[4][3], Is.EqualTo(9));   
+    }
+
+    [Test] 
+    public void FindVisibleTreesTest() 
+    {
+        string[] input = {
+            "30373",
+            "25512",
+            "65332",
+            "33549",
+            "35390"
+        };
+        List<int[]> ints = Program.StringArrayTo2DInts(input);
+        int result = Program.FindVisibleTrees(ints);
+        Assert.That(result, Is.EqualTo(21));
+    }
+
+    [TestCase(1,1,true)]
+    [TestCase(3,3,false)]
+    public void CheckVisibilityTest(int targetX, int targetY, bool expectedResult)
+    {
+        string[] input = {
+            "30373",
+            "25512",
+            "65332",
+            "33549",
+            "35390"
+        };
+        List<int[]> ints = Program.StringArrayTo2DInts(input);
+        bool result = Program.CheckVisibility(ints, targetX, targetY);
+        Assert.That(result, Is.EqualTo(expectedResult));
+    }
+}
